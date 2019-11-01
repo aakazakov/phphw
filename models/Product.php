83af6844d0b5aceb51afdf977564ca49f3aaa0e0
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-01 15:41:25 +0300

    wrote some code

--- models/Product.php
+++ models/Product.php
@@ -1,3 +1,11 @@
 <?php
 
 declare(strict_types=1);
+
+class Product
+{
+    private $id;
+    private $name;
+    private $description;
+    private $price;
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
 declare(strict_types=1);
+
+include '../engine/Autoload.php';
+
+function __autoload($className)
+{
+    (new Autoload())->loadClass($className);
+}
+
+$product - new Product();
+
+var_dump($product);

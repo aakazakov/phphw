a69ba503027af011aa0a5f61ddd880ef3b2564d8
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-01 16:16:43 +0300

    wrote some code

--- engine/Autoload.php
+++ engine/Autoload.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 class Autoload
 {
+    private $path = [
+        'models',
+        'engine'
+    ];
+
     public function LoadClass($className)
     {
-        include "../models/{$className}.php";
+        foreach ($this->path as $path) {
+            $fileName = "../{$path}/{$className}.php";
+            if (file_exists($fileName)) {
+                include $fileName;
+                break;
+            }
+        }
     }
 }

3febedd28f5e4ae3d852210e47c3ded6aa95eaac
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-01 21:24:34 +0300

    wrote some code

--- engine/db.php
+++ engine/db.php
@@ -4,5 +4,13 @@ declare(strict_types=1);
 
 class Db
 {
-    
+    public function queryOne(string $sql, array $params = []) : string
+    {
+        return $sql;
+    }
+
+    public function queryAll(string $sql, array $params = []) : string
+    {
+        return $sql;
+    }
 }

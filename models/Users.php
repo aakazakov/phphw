08374297161b7396a24ce555cb55d271bee916cb
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-01 22:43:30 +0300

    wrote some code

--- models/Users.php
+++ models/Users.php
@@ -2,9 +2,14 @@
 
 declare(strict_types=1);
 
-class Users
+class Users extends Model
 {
     private $id;
     private $login;
     private $pass;
+
+    public function getTableName() : string
+    {
+        return 'users';
+    }
 }

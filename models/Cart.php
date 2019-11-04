d901fabe897ea6a4c480e873a9e5f06af39af7a5
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-04 10:31:56 +0300

    added class Cart

---
+++ models/Cart.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\models;
+
+use app\models\Model;
+
+class Cart extends Model
+{
+    protected $id;
+    protected $session_id;
+    protected $goods_id;
+
+    public function getTableName() : string
+    {
+        return 'goods';
+    }
+}

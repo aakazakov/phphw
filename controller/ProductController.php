9394936de5fa0a04fbf64ca679d374b859eeca1c
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-12 23:04:28 +0300

    added controller

---
+++ controller/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controller;
+
+class ProductController
+{
+    public function actionCatalog()
+    {
+
+    }
+
+    public function actionCard()
+    {
+
+    }
+}
--- public/index.php
+++ public/index.php
@@ -10,10 +10,16 @@ use app\engine\Autoload;
 
 spl_autoload_register([new Autoload, 'LoadClass']);
 
+$controllerName = $_GET['c'] ? : 'product';
+$controllerAction = $_GET['a'];
+$controllerClass = "app\\controller\\" . ucfirst($controllerName) . "Controller";
+var_dump($controllerClass);
+
+/*
 $product = new Product('Пицца', 'Ассорти, 32см', 300);
 $product->save();
-// $product->doDelete();
-// $product = Product::getOne(71);
-
-// (new Product('Пицца', 'Ассорти, 32см', 300))->doInsert();
+$product->doDelete();
+$product = Product::getOne(1);
+(new Product('Пицца', 'Ассорти, 32см', 300))->doInsert();
 var_dump($product);
+*/

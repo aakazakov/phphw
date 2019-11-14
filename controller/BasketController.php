6ef8da7723ba3a3bd66e5e7e8cb4d1c69d17ffe4
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-14 16:16:59 +0300

    abracadabra)

---
+++ controller/BasketController.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controller;
+
+class BasketController
+{
+    private $action;
+    private $defaultAction = 'emptyBasket';
+    private $layout = 'main';
+    private $useLayout = true;
+
+    public function runAction($action = null)
+    {
+        $this->action = $action ? : $this->defaultAction;
+        $method = 'action' . ucfirst($this->action);
+        if (method_exists($this, $method)) {
+            $this->$method();
+        } else {
+            echo 'Bad news: 404 (no action)'; 
+        }
+    }
+
+    public function render($templateName, $params = [])
+    {
+        if ($this->useLayout) {
+            return $this->renderTemplate( "layouts/{$this->layout}", [
+                    'menu' => $this->renderTemplate('menu'),
+                    'content' => $this->renderTemplate($templateName, $params)
+                ]
+            );
+        } else {
+            return $this->renderTemplate($templateName, $params = []);
+        }
+    }
+
+    public function renderTemplate($templateName, $params = [])
+    {
+        ob_start;
+        extract($params);
+        $templatePath = TEMPLATES_DIR . $templateName . '.php';
+        if (file_exists($templatePath)) {
+            include $templatePath;
+        }
+        ob_get_clean;
+    }
+
+    public function actionEmptyBasket()
+    {
+        echo $this->render('basket');
+        echo 'Ваша корзина пуста';
+    }
+
+    public function actionOrders()
+    {
+        echo $this->render('basket');
+        echo 'Ваши заказы:';
+    }
+}
--- templates/menu.php
+++ templates/menu.php
@@ -1,4 +1,5 @@
 <div style="padding: 10px">
     <a style="padding: 0px 5px" href="/">Главная</a>
     <a style="padding: 0px 5px" href="/product/catalog/">Каталог</a>
+    <a style="padding: 0px 5px" href="/basket/">Корзина</a>
 </div>

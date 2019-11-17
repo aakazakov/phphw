2000fa26081954cfb9349af89d701f25f91d0fd4
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-17 21:51:19 +0300

    alternative foreach syntax

--- templates/catalog.php
+++ templates/catalog.php
@@ -1,12 +1,10 @@
 <h2>Каталог</h2>
 <section style="display: grid; grid-template-columns: repeat(5, 1fr); grid-gap: 5px;">
-<?php
-    foreach ($catalog as $value) {
-        echo   "<div style=\"display: flex; justify-content: center; padding: 5px; background-color: grey;\">
-                    <a href=\"/product/card/?id={$value['id']}\">
-                        <img src=\"https://placehold.co/180?text={$value['name']}\">
-                    </a>
-                </div>";
-    }
-?>
+<?foreach ($catalog as $value):?>
+    <div style="display: flex; justify-content: center; padding: 5px; background-color: grey;">
+        <a href="/product/card/?id=<?=$value['id']?>">
+            <img src="https://placehold.co/180?text=<?=$value['name']?>">
+        </a>
+    </div>
+<?endforeach;?>
 </section>

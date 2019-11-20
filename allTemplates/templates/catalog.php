1485e46b911a640121bfdbe79f8d76977c7955dd
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-20 15:43:16 +0300

    make-up

--- allTemplates/templates/catalog.php
+++ allTemplates/templates/catalog.php
@@ -1,10 +1,11 @@
 <h2>Каталог</h2>
-<section style="display: grid; grid-template-columns: repeat(5, 1fr); grid-gap: 5px;">
+<section style="display: grid; grid-template-columns: repeat(5, 1fr); grid-gap: 5px; max-width: 70%;">
 <?foreach ($catalog as $value):?>
-    <div style="display: flex; justify-content: center; padding: 5px; background-color: grey;">
+    <div style="display: flex; flex-direction: column; align-items: center; padding: 5px; background-color: grey;">
         <a href="/product/card/?id=<?=$value['id']?>">
             <img src="https://placehold.co/180?text=<?=$value['name']?>">
         </a>
+        <button data-id="<?=$value['id']?>">Купить</button>
     </div>
 <?endforeach;?>
 </section>

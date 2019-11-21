172dbed09ddff6942d0a86edc83aeab199e3369c
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-21 10:08:06 +0300

    js code has been updated

--- allTemplates/templates/card.php
+++ allTemplates/templates/card.php
@@ -3,5 +3,5 @@
     <h2><?=$product->name?></h2>
     <p><?=$product->description?></p>
     <span><?=$product->price?></span> р.
-    <button style="margin-left: 5px; padding: 2px 5px;">Купить</button>
+    <button class="buy" data-id="<?=$product->id?>" style="margin-left: 5px; padding: 2px 5px;">Купить</button>
 </div>
--- allTemplates/templates/catalog.php
+++ allTemplates/templates/catalog.php
@@ -1,5 +1,5 @@
 <h2>Каталог</h2>
-<section style="display: grid; grid-template-columns: repeat(5, 1fr); grid-gap: 5px; max-width: 70%;">
+<section class="catalog" style="display: grid; grid-template-columns: repeat(5, 1fr); grid-gap: 5px; max-width: 70%;">
 <?foreach ($catalog as $value):?>
     <div style="display: flex; flex-direction: column; align-items: center; padding: 5px; background-color: grey;">
         <a href="/product/card/?id=<?=$value['id']?>">
@@ -11,26 +11,29 @@
 </section>
 
 <script>
-    const buyButtons = [...document.querySelectorAll('.buy')];
+    'use strict';
 
-    buyButtons.forEach((el) => {
-        el.addEventListener('click', () => {
-            const id = el.getAttribute('data-id');
-            (
-                async () => {
-                    const response = await fetch('/basket/addToBasket/', {
-                        method: 'POST',
-                        headers: new Headers({
-                            'Content-Type': 'application/json'
-                        }),
-                        body: JSON.stringify({
-                            'id': id
-                        })
-                    });
-                    const answer = await response.json();
-                    console.log(answer.response, answer.id);
-                }
-            )();
-        });
-    });
+    const buy = (event) => {
+        if (event.target.tagName !== 'BUTTON') {
+            return;
+        }
+        const id = event.target.dataset.id;
+        (
+            async () => {
+            const response = await fetch('/basket/addToBasket/', {
+                method: 'POST',
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                }),
+                body: JSON.stringify({
+                    'id': id
+                })
+            });
+            const answer = await response.json();
+            console.log(answer.response, answer.id);
+        }
+        )();
+    };
+
+    document.querySelector('.catalog').addEventListener('click', buy);
 </script>

0fde4e1192628f8922c0d4e5ebe47990e94f2fdb
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-25 11:00:13 +0300

    fix js

--- allTemplates/templates/basket.php
+++ allTemplates/templates/basket.php
@@ -1,7 +1,7 @@
 <h2>Корзина</h2>
 <section class="del-btns" style="display: grid; grid-template-columns: repeat(5, 1fr); grid-gap: 5px; max-width: 70%;">
 <?foreach ($products as $value):?>
-    <div style="display: flex; flex-direction: column; align-items: center; padding: 5px; background-color: grey;">
+    <div id="<?=$value['id_basket']?>" style="display: flex; flex-direction: column; align-items: center; padding: 5px; background-color: grey;">
         <a href="/product/card/?id=<?=$value['id_prod']?>">
             <img src="https://placehold.co/180?text=<?=$value['name']?>">
         </a>

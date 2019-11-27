b246427f6cf2ff0560e5c78c19fddc4bd3aae543
andrey_kazakov <aakazakovv@yandex.ru>
2019-11-27 14:54:29 +0300

    loading by Composer

--- public/index.php
+++ public/index.php
@@ -3,13 +3,12 @@
 declare(strict_types=1);
 
 use app\engine\{App, Autoload, AutoloadException};
-// use app\models\{Product, Users, Basket};
 
 // try {
     session_start();
 
     include realpath('../vendor/autoload.php');
-    include realpath('../engine/Autoload.php');
+    // include realpath('../engine/Autoload.php');  // Loading by Composer.
     include realpath('../config/config.php');
     
     spl_autoload_register([new Autoload, 'LoadClass']);

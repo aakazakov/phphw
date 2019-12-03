198dc04b48d6d1c1cbdaed72b8d7ba2a478bc764
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-03 16:54:42 +0300

    fix logout

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -6,6 +6,7 @@ namespace app\controller;
 
 use app\engine\App;
 use app\controller\Controller;
+use app\models\entities\Basket;
 
 class AuthController extends Controller
 {
@@ -27,6 +28,9 @@ class AuthController extends Controller
 
     public function actionlogout()
     {
+        if (App::call()->basketRepository->getCountWhere('session_id', session_id())) {
+            App::call()->basketRepository->doDeleteBySession(new Basket(session_id()));
+        }
         session_regenerate_id();
         session_destroy();
         header("location: /");

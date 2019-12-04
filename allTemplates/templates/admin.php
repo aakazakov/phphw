490a9b657d517ce013360f0c9270290239df0f7e
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-04 18:46:51 +0300

    updated make-up

--- allTemplates/templates/admin.php
+++ allTemplates/templates/admin.php
@@ -2,7 +2,7 @@
 
 <section class="status-btns" style="padding: 1vw 0;">
 <?foreach ($orders as $value):?>
-    <div style="padding: 1vw 0;">
+    <div style="padding: 1vw; border: 0.1vw dotted grey; width: fit-content;">
         <a href="/orders/anOrder/?sid=<?=$value['session_id']?>">Заказ #<?=$value['id']?></a> | <span>Статус: <?=$value['status']?></span> |
         <form action="/orders/status/?id=<?=$value['id']?>" method="post" style="display: flex; align-items: center;">
             <p>Новый статус:
@@ -14,6 +14,7 @@
             </p>
                 <button type="submit">Ок</button>
         </form>
+        <div>Kлиент: <?=$value['user_name']?> | E-mail: <?=$value['user_email']?></div>
     </div>
 <?endforeach;?>
 </section>

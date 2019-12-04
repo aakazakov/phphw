f38f8f2b8ab76cbf94afaa92eb0afe5c8d8ac945
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-04 15:51:04 +0300

    template ready to use

--- allTemplates/templates/admin.php
+++ allTemplates/templates/admin.php
@@ -1,1 +1,19 @@
-<strong>admin's panel is here</strong>
+<strong>| Панель администратора |</strong>
+
+<section class="status-btns" style="padding: 1vw 0;">
+<?foreach ($orders as $value):?>
+    <div style="padding: 1vw 0;">
+        <a href="/orders/anOrder/?sid=<?=$value['session_id']?>">Заказ #<?=$value['id']?></a> | <span>Статус: <?=$value['status']?></span> |
+        <form action="/orders/status/?id=<?=$value['id']?>" method="post" style="display: flex; align-items: center;">
+            <p>Новый статус:
+                <select name="status">
+                    <option value="оплачен">оплачен</option>
+                    <option value="в работе">в работе</option>
+                    <option value="обработан">обработан</option>
+                </select>
+            </p>
+                <button type="submit">Ок</button>
+        </form>
+    </div>
+<?endforeach;?>
+</section>

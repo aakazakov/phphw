d8e375c752c344b290a7460a1863037dd59ab354
andrey_kazakov <aakazakovv@yandex.ru>
2019-12-04 20:48:51 +0300

    updated make-up

--- allTemplates/templates/anOrder.php
+++ allTemplates/templates/anOrder.php
@@ -1,1 +1,5 @@
-<strong>Заказ # </strong>
+<?foreach ($goods as $key => $value):?>
+    <div style="border-bottom: 0.1vw dotted grey; width: fit-content; padding: 0.2vw 0">
+        <?=$key+1?>. <?=$value->name?> | Цена: <?=$value->price?> р. | Описание: <?=$value->description?>.
+    </div>
+<?endforeach;?>
